6eea1ebffb01a827d95bc5097ac2239879222c9c
Mario Moreno <66931789+mmorenodev@users.noreply.github.com>
2024-01-30 22:46:55 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
@@ -7,11 +8,11 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer,{
     cors: {
-        origin: "http://localhost:8069", // Ajusta esto según sea necesario
-        methods: ["GET", "POST"]
+      origin: "*",
+      methods: ["GET", "POST"]
     }
-});
-
+  });
+app.use(cors());
 app.use( express.static(path.join(__dirname, "views")) );
 
 app.get("/", (req, res) => {
@@ -19,24 +20,34 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", socket => {
-
+    console.log(`Cliente conectado: ${socket.id}`);
     // Emisión básica
     socket.emit("welcome", "Ahora estás conectado 😎.");
 
     socket.on("server", data => {
         console.log(data);
+        io.emit("ping", "ping desde el server")
     });
 
-    socket.on("lee_peso_bascula", message => {
-
-        console.log(message);
-        socket.emit("peso_leido","120 KG");
+    socket.on("lee_peso", data => {
+        console.log("LEE PESO EN BASCULA");
+        console.log(data);
+        // Emitir a todos los clientes
+        io.emit("lee_peso", "Leer Peso Bascula")
     
     });
 
+    socket.on("peso_leido", data => {
+        console.log("PESO LEIDO EN BASCULA");
+        console.log(data);
+        io.emit("peso_leido", data)
+    });
+
+
+
     // Emisión a todos
     io.emit("everyone", socket.id + " se ha conectado 👀");
-
 });
 
-httpServer.listen(3000);
+
+httpServer.listen(process.env.PORT || 3000);
